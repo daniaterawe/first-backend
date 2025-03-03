373bab119a09a76ebf478877ba2aedbbac2c386f
daniaterawe <danianabeel7@gmail.com>
2025-03-03 14:18:16 +0300

    task3

--- task2/jd.js
+++ task2/jd.js
@@ -1,20 +1,52 @@
-let students =[];
-function createStudent(name,age,mark){
-    return{name:name,age:age,mark:mark};
-}
-students.push(createStudent("dania",20,95));
-students.push(createStudent("ali",20,30));
-students.push(createStudent("rami",20,78));
-students.push(createStudent("deema",20,82));
-students.push(createStudent("osama",20,70));
-let sucssesStudents=[];
-let failedStudents=[];
-for(let i=0;i<students.length;i++){
-if (students[i].mark>=50) {
-    sucssesStudents.push(students[i]);
-} else {
-   failedStudents.push(students[i]) ;
-}}
-console.log(sucssesStudents);
-console.log(failedStudents);
+const express = require("express");
+const app = express();
 
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
+let arr = [
+  { id: 1, username: "khattab", age: 20 },
+  { id: 2, username: "rami", age: 20 },
+  { id: 3, username: "batool", age: 20 }
+];
+
+// GET all users
+app.get("/users", (req, res) => {
+  res.send(arr);
+});
+
+// POST a new user
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+  arr.push(newUser);
+  res.send("User created");
+});
+
+// PUT update a user
+app.put("/users", (req, res) => {
+  const id = req.body.id;
+  let userFound = false;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].id === id) {
+      arr[i] = req.body;
+      userFound = true;
+      break;
+    }
+  }
+
+  if (userFound) {
+    res.send("User updated");
+  } else {
+    res.status(404).send("User not found");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
+app.delete("/users/id",(req,res)=>{
+  for(let i=0;i<arr.length;i++){
+const newArr=arr.filter((item)=>item.id!==id)
+res.send(newArr)
+  }})
